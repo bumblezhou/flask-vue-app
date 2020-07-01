b85e671ac3a07d209f0cf64f06d56be797975e4e
Jack Zhou <bumble.zhou@gmail.com>
2020-07-01 15:31:44 +0800

    static folder and templates

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,21 @@
 from flask import Flask, jsonify, make_response, render_template
-from flask_cors import CORS
+import uuid
+# from flask_cors import CORS
 
 DEBUG = True
 
-app = Flask(__name__, #static_url_path='',
-        template_folder='static')
-        # static_folder='./static')
-app.config.from_object(__name__)
-
-CORS(app, resources={r'/*': {'origins': '*'}})
+app = Flask(__name__, template_folder='templates', static_folder="static")
+# CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-@app.route('/api/ping', methods=['GET'])
+@app.route('/api/ping', methods=['GET', 'POST'])
 def ping_pong():
     return jsonify('pong!')
 
+
 @app.route('/')
 def index():
-    return make_response(render_template('/index.html'))
+    return make_response(render_template('index.html', name="test_user", token=uuid.uuid1()))
     # return app.send_static_file('index.html')
 
 # @app.errorhandler(404)
@@ -28,15 +26,15 @@ def index():
 @app.route("/<path:fallback>")
 def fallback(fallback):
     print('fallback = ' + fallback)
-    if fallback.startswith('css/') or fallback.startswith('js/') \
-        or fallback.startswith('img/') or fallback == 'favicon.ico':
-            #return make_response(render_template('/' + fallback))
-            return app.send_static_file(fallback)
+    if fallback.startswith('css/') or fallback.startswith('js/') or fallback.startswith('img/') or fallback == 'favicon.ico':
+        # return make_response(render_template('/' + fallback))
+        return app.send_static_file(fallback)
     else:
-        #return make_response(render_template('/index.html'))
+        # return make_response(render_template('/index.html'))
         return app.send_static_file('index.html')
-    #resp = make_response(render_template("/index.html"))
-    #return resp
+    # resp = make_response(render_template("/index.html"))
+    # return resp
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, threaded=True)
